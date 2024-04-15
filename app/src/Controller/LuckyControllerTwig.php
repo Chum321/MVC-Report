c6dab10d3b92f6396c74bbf0f731ab66b74c1906
Chum321 <timlundqvist2002@mail.com>
2024-04-15 19:32:37 +0200

    Flyttade routen api/quotes till Json controllern och fixade navbar länk och raporten för kmom01

--- app/src/Controller/LuckyControllerTwig.php
+++ app/src/Controller/LuckyControllerTwig.php
@@ -118,27 +118,5 @@ class LuckyControllerTwig extends AbstractController
         return false;
     }
 
-    #[Route("/api/quote", name: "api_quote")]
-    public function apiQuote(): JsonResponse
-    {
-        $quotes = [
-            "The only way to do great work is to love what you do. - Steve Jobs",
-            "In the midst of chaos, there is also opportunity. - Sun Tzu",
-            "These quotes was not taken from chatgpt 3.5, totes not."
-        ];
-
-        // Selects a random quote
-        $randomQuote = $quotes[array_rand($quotes)];
-
-        $date = date("Y-m-d");
-        $timestamp = time();
-
-        $response = new JsonResponse([
-            'quote' => $randomQuote,
-            'date' => $date,
-            'timestamp' => $timestamp
-        ]);
-
-        return $response;
-    }
+    
 }
