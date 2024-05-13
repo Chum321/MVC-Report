3f8e307df81929b31fbd501175a0e2ece0de3df4
Chum321 <timlundqvist2002@mail.com>
2024-05-13 14:42:24 +0200

    Made page for card/deck, card/deck/shuffle and card/deck/draw. Also made the routes for them. Moved card_landing into templates/card/ folder

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -19,8 +19,43 @@ class CardGameController extends AbstractController
     #[Route("/card", name: "card_landingpage")]
     public function landingPage(): Response
     {
+
+        // Check if session exists
+        //code provided by chatgpt for starting session if it is not on, stored here on temp basis
+        // if (!$session->isStarted()) {
+            // Start the session
+        //     $session->start();
+        // }
+
         $currentYear = date('Y');
-        return $this->render('card_landingpage.html.twig', [
+        return $this->render('card/card_landingpage.html.twig', [
+            'currentYear' => $currentYear,
+        ]);
+    }
+
+    #[Route("/card/deck", name: "deck_page")]
+    public function deck(): Response
+    {
+        $currentYear = date('Y');
+        return $this->render('card/card_deck.html.twig', [
+            'currentYear' => $currentYear,
+        ]);
+    }
+
+    #[Route("/card/deck/shuffle", name: "shuffle_page")]
+    public function shuffle(): Response
+    {
+        $currentYear = date('Y');
+        return $this->render('card/card_shuffle.html.twig', [
+            'currentYear' => $currentYear,
+        ]);
+    }
+
+    #[Route("/card/deck/draw", name: "draw_page")]
+    public function draw(): Response
+    {
+        $currentYear = date('Y');
+        return $this->render('card/card_draw.html.twig', [
             'currentYear' => $currentYear,
         ]);
     }
